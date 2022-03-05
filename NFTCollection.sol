79bcae5d4c75f5f48c91236c88e7cd3b918f6215
Alexander <alexanderdcastaneda@utexas.edu>
2022-03-05 10:52:38 -0600

    added functions in NFTCollection

--- NFTCollection.sol
+++ NFTCollection.sol
@@ -8,6 +8,7 @@ contract NFTCollection{
 		string eid;
 		address walletAddress;
 		uint NftNum;   //unique number for each NFT perhaps
+		boolean validMember;
 	}
 
 	mapping (address => ID) members; //key is a wallet, value is ID
@@ -22,11 +23,33 @@ contract NFTCollection{
 				eid: _eid;
 				walletAddress: msg.sender; //not sure
 				NftNum = collectionOwners.length;
+				validMember = true;
 			}
 		);
 		collectionOwners.push(msg.sender);
 		
 		
-	} 
+	}
+
+	function invalidateMember(address _address, string memory _eid) {
+		
+		require(members[_address] != null, "This is not a current member.");
+		require(members[_address].eid == _eid, "EID's do not match.");  //hash??
+		
+		members[_address].validMember = false; 
+		
+		//maybe an emit here to state something has been invalidated	
+	}
+	
+	function checkMember(string memory _name,string memory _eid) public view returns(boolean){
+		require(members[msg.sender] != null, "This is not a current member.");
+		
+		require(members[msg.sender].name == _name, "Name does not match.");
+		require(rembers[msg.sender].eid == _eid, "EID does not match.");
+		require(members[msg.sender].validMember, "This member is terminated.");
+				
+		//emit something for frontend
+		return true;					
+	}
 }
 
